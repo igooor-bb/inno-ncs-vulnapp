629893ecd2bed034d7e1d1b7a8fc8740454bd9a6
Igor Belov <igooor.ww@gmail.com>
2022-04-18 20:01:27 +0300

    Fix vulnerabilities

--- app/address/views.py
+++ app/address/views.py
@@ -3,16 +3,18 @@ from django.shortcuts import render
 from .models import AddressModel
 from .forms import SearchForm
 
+import bleach
+
 def search_address(request):
     context = {}
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['query']
-            query = f"SELECT * FROM address_addressmodel WHERE address LIKE '%%{text}%%'"
-            result = AddressModel.objects.raw(query)
+            result = AddressModel.objects.filter(address__contains=text)
             if result is not None and len(result) > 0:
-                context['result'] = result
+                address_list = [bleach.clean(x.address, strip=True) for x in result]
+                context['result'] = address_list
             else:
                 context['not_found'] = True
     else:
